4d675f1df3fc4f775aee02dcf20fe54e0444f856
Vira Voronova <vera.voronova@hotmail.com>
2022-12-21 12:06:42 +0100

    first route made

---
+++ index.js
@@ -0,0 +1,24 @@
+// const express = require('express');
+// const morgan = require('morgan');
+// require('dotenv').config();
+// const { postsRouter } = require('./routers/postsRouter');
+
+import express from 'express';
+import morgan from 'morgan'; // middleware for logging
+import postsRouter from './routers/postsRouter.js'; // router with posts
+import * as dotenv from 'dotenv'; // to get variables from .env
+dotenv.config();
+
+const app = express(); // initialization app
+
+const PORT = process.env.PORT || 8081; // get const from .env config
+
+app.use(express.json()); // parsing JSON
+app.use(morgan('tiny')); // morgan logger type
+
+app.use('/api/posts', postsRouter); // add postsRouter to app
+
+app.listen(PORT, err => {
+  if (err) console.log('error st server launch', err);
+  console.log(`server works at port ${PORT}`);
+});
